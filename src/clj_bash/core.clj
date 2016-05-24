0e391d7cb3179082c2e814a2ab868c1a10744487
eshamster <hamgoostar@gmail.com>
2016-05-25 00:24:00 +0900

    Add the most basic prototype

--- src/clj_bash/core.clj
+++ src/clj_bash/core.clj
@@ -1,6 +1,26 @@
 (ns clj-bash.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(use '[clojure.string :only [join]])
+(use '[clojure.core.match :only [match]])
+
+(defn- parse-command [command rest]
+  (concat (list :bash command) (map name rest)))
+
+(defn parse-main [line]
+  (if (keyword? (first line))
+    (parse-command (name (first line)) (rest line))
+    (eval line)))
+
+(defmacro do-dsl [& body]
+  `(map parse-main '~body))
+
+(defn pprint-tree [tree]
+  (doseq [line tree]
+    (match [line]
+           [([:bash & rest] :seq)] (println (join " " rest)))))
+
+(defn -main [& args]
+  (pprint-tree
+   (do-dsl
+    (:cd test)
+    (:ls .))))
