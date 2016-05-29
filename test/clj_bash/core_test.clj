1eaa0fb499dd8f357fb51a56890cfc85b5de4c5d
eshamster <hamgoostar@gmail.com>
2016-05-29 22:54:55 +0900

    Add test for 'for' clause

--- test/clj_bash/core_test.clj
+++ test/clj_bash/core_test.clj
@@ -7,4 +7,8 @@
   (test-bash simple-execution
              (:echo 0)
              (:echo -n "in _")
-             (:echo test)))
+             (:echo test))
+  (test-bash for-array
+             (for i [0 4 3 5] (:echo $i)))
+  (test-bash for-expression
+             (for count (:seq 0 5) (:echo $count))))
