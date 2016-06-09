7a630e9a6de873bbb4cee9e047f8730d312149fb
eshamster <hamgoostar@gmail.com>
2016-06-10 00:32:11 +0900

    Implement str-bash.str-body/concat-str-body

--- src/clj_bash/str_body.clj
+++ src/clj_bash/str_body.clj
@@ -8,6 +8,35 @@
            (string? (first elem))
            (string? (last elem)))))
 
+;; Ex. "test" "abc"                           -> "testabc"
+;;     "test" ("abc" (0) "111")               -> ("testabc" (0) "111")
+;;     ("abc" (0) "111") "test"               -> ("abc" (0) "111test")
+;;     ("abc" (0) "111") ("test" '(1) "abcd") -> ("abc" (0) "111test" (quote (1)) "abcd")
+(defn- concat-two-str-body [left right]
+  (when-not (and (str-body? left) (str-body? right))
+    (throw (IllegalArgumentException. "left and right should include only str-bodies")))
+  (cond
+    (and (string? left)
+         (string? right))
+    (str left right)
+    (and (string? left)
+         (not (string? right)))
+    `(~(str left (first right)) ~@(rest right))
+    (and (not (string? left))
+         (string? right))
+    `(~@(butlast left) ~(str (last left) right))
+    (and (not (string? left))
+         (not (string? right)))
+    `(~@(butlast left) ~(str (last left) (first right)) ~@(rest right))))
+
+(defn concat-str-body [left & rest-str-body-lst]
+  (loop [head left
+         rest-lst rest-str-body-lst]
+    (if-not (empty? rest-lst)
+      (recur (concat-two-str-body head (first rest-lst))
+             (rest rest-lst))
+      head)))
+
 ;; Ex. "[" ("str") "]" -> ("[str]")
 ;; Ex. "[" ("do" ("a" "b") "done") "]" -> ("[do" ("a" "b") "done]")
 ;; TODO: Fix because the definition of str-body is changed to accept a string 
--- test/clj_bash/str_body_test.clj
+++ test/clj_bash/str_body_test.clj
@@ -14,6 +14,19 @@
     '("test" ("test2"))
     '(("test2") "test")))
 
+(deftest concat-str-body-test
+  (are [args expected] (= (apply concat-str-body args) expected)
+    '("test") "test"
+    '(("a" (1) "b")) '("a" (1) "b")
+    '("test" " | " "abc") "test | abc"
+    '("test" ("a" (1) "b")) '("testa" (1) "b")
+    '(("a" (1) "b") "test") '("a" (1) "btest")
+    '(("a" (1) "b") ("c" (2) "d")) '("a" (1) "bc" (2) "d")
+    '(("a" (1) "b") " > " ("c" (2) "d")) '("a" (1) "b > c" (2) "d"))
+  (testing "Wrong arguments"
+    (is (thrown? Exception (concat-str-body)))
+    (is (thrown? IllegalArgumentException (concat-str-body "test" 0 '("ab" "cd"))))))
+
 (deftest wrap-str-body-test
   (are [left body right expected]
       (= (wrap-str-body left body right)
